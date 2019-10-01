eee3627aaeed4f17736809fdc0acc6cc80da588d
Shehnaz <shehnazshaik7@gmail.com>
2019-10-01 18:16:42 +0530

    Added comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,16 +12,21 @@ export class AppComponent implements OnInit {
   title = 'task';
   
   rowValues = []
-  ngOnInit() { 
-    this.rowValues = this.getRowData();
-  }
+ 
   getHeaders() {
+    // returning the values of an object to tabs
     return Object.keys(mainData.data)
   }
   getTableHeaders(header) {
+    // retuning list of values to the header
     return mainData.data[header]
   }
   getRowData() {
+    // returning an object (data) 
     return subData.Data;
+  }
+  ngOnInit() { 
+    //assigning an object to the table 
+    this.rowValues = this.getRowData();
   }
 }
